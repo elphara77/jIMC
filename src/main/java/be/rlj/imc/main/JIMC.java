c219fcfafb18d7e247f641f91d6063898a359e82
Raphaël Laporte <elphara@skynet.be>
2016-11-03 22:58:32 +0100

    +indication de surcharge

---
+++ src/main/java/be/rlj/imc/main/JIMC.java
@@ -0,0 +1,33 @@
+package be.rlj.imc.main;
+
+public class JIMC {
+
+    private final static double IDEAL = 25.;
+
+    public static void main(String[] args) {
+        double imc = -1.;
+        double tailleIdeal = -1;
+
+        double poids = 54.;
+        double taille = 1.65;
+        System.out.println(String.format("Vous pesez %1$.2f Kg pour %2$.2f m", poids, taille));
+
+        imc = poids / (taille * taille);
+        tailleIdeal = IDEAL * taille * taille;
+
+        if (imc < 0 || tailleIdeal < 0) {
+            System.err.println("Erreur avec le calcul de votre IMC !!!");
+        } else {
+            System.out.println(String.format("Votre IMC est de %1$.2f et votre poids idéal est de %2$.2f Kg (calcul sur l'IMC idéal de %3$.2f)", imc, tailleIdeal, IDEAL));
+            double surcharge = poids - (IDEAL * taille * taille);
+            if (Math.round(surcharge) > 0) {
+                System.out.println(String.format("Vous êtes en SUR-charge pondérale de %1$.2f Kg :-( !", surcharge));
+            } else if (Math.round(surcharge) < 0) {
+                System.out.println(String.format("Vous êtes en SOUS-charge pondérale de %1$.2f Kg :-( !", -surcharge));
+            } else {
+                System.out.println(String.format("Vous êtes en forme :-)", surcharge));
+            }
+        }
+    }
+
+}
---
+++ src/main/java/be/rlj/imc/main/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * jIMC
+ *
+ * calcul sur l'indice de masse corporelle (français)
+ *
+ * @author rl
+ *
+ */
+package be.rlj.imc.main;
