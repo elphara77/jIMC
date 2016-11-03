056109b0c342195d6a9469e39f2a38307e67cb9c
Raphaël Laporte <elphara@skynet.be>
2016-11-03 23:23:12 +0100

    +inputs

--- src/main/java/be/rl/j/imc/main/JIMC.java
+++ src/main/java/be/rl/j/imc/main/JIMC.java
@@ -1,36 +1,65 @@
 package be.rl.j.imc.main;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class JIMC {
 
     private final static double IDEAL = 25.;
+    private final static String TITLE = "Calcul sur l'indice de masse corporelle (français)";
 
     public static void main(String[] args) {
-        double imc = -1.;
-        double tailleIdeal = -1;
+        do {
+            System.out.println(TITLE.replaceAll(".", "-"));
+            System.out.println(TITLE);
+            System.out.println(TITLE.replaceAll(".", "-"));
 
+            double imc = -1.;
+            double tailleIdeal = -1;
 
-        //Coucou Jamal Melhaoui Champion Boxe & Cie Marroco 1997
-        double poids = 54.;
-        double taille = 1.65;
+            // Coucou Jamal Melhaoui Champion Boxe & Cie Marroco 1997
+            double poids = 54.;
+            double taille = 1.65;
 
-        System.out.println(String.format("Vous pesez %1$.2f Kg pour %2$.2f m", poids, taille));
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(System.in);
+                System.out.print("Voutre poids en Kg ? ");
+                String poidsStr = scanner.next("\\d+\\.?\\,?\\d*");
+                System.out.print("Voutre taille en cm ? ");
+                String tailleStr = scanner.next("\\d+\\.?\\,?\\d*");
 
-        imc = poids / (taille * taille);
-        tailleIdeal = IDEAL * taille * taille;
+                poids = Double.parseDouble(poidsStr.replace(",", "."));
+                taille = Double.parseDouble(tailleStr.replace(",", ".")) / 100.;
 
-        if (imc < 0 || tailleIdeal < 0) {
-            System.err.println("Erreur avec le calcul de votre IMC !!!");
-        } else {
-            System.out.println(String.format("Votre IMC est de %1$.2f et votre poids idéal est de %2$.2f Kg (calcul sur l'IMC idéal de %3$.2f)", imc, tailleIdeal, IDEAL));
-            double surcharge = poids - (IDEAL * taille * taille);
-            if (Math.round(surcharge) > 0) {
-                System.out.println(String.format("Vous êtes en SUR-charge pondérale de %1$.2f Kg :-( !", surcharge));
-            } else if (Math.round(surcharge) < 0) {
-                System.out.println(String.format("Vous êtes en SOUS-charge pondérale de %1$.2f Kg :-( !", -surcharge));
-            } else {
-                System.out.println(String.format("Vous êtes en forme :-)", surcharge));
+                System.out.println(String.format("Vous pesez %1$.2f Kg pour %2$.2f m", poids, taille));
+
+                imc = poids / (taille * taille);
+                tailleIdeal = IDEAL * taille * taille;
+
+                if (imc < 0 || tailleIdeal < 0) {
+                    System.err.println("Erreur avec le calcul de votre IMC !!!");
+                } else {
+                    System.out.println(String.format("Votre IMC est de %1$.2f et votre poids idéal est de %2$.2f Kg (calcul sur l'IMC idéal de %3$.2f)", imc, tailleIdeal, IDEAL));
+                    double surcharge = poids - (IDEAL * taille * taille);
+                    if (Math.round(surcharge) > 0) {
+                        System.out.println(String.format("Vous êtes en SUR-charge pondérale de %1$.2f Kg :-( !", surcharge));
+                    } else if (Math.round(surcharge) < 0) {
+                        System.out.println(String.format("Vous êtes en SOUS-charge pondérale de %1$.2f Kg :-( !", -surcharge));
+                    } else {
+                        System.out.println(String.format("Vous êtes en forme :-)", surcharge));
+                    }
+                    System.out.println();
+                    System.out.println();
+                }
+            } catch (Throwable t) {
+                System.err.println("Erreur avec le calcul de votre IMC !!!");
+                System.err.printf("Erreur : %s", t.getMessage());
+            } finally {
+                if (scanner != null) {
+                    scanner.reset();
+                }
             }
-        }
+        } while (true);
     }
-
 }
